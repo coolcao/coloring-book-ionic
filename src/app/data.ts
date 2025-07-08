4a1e81f0eb716eca60c554de7c42b7de8ee6a487
coolcao <me@coolcao.com>
2025-07-08 12:09:49 +0800

    update

--- src/app/data.ts
+++ src/app/data.ts
@@ -2,7 +2,7 @@ export interface Data {
   id: string;
   name: string;
   image: string;
-  category: '动物' | '食物' | '人物' | '玩具';
+  category: '动物' | '水果' | '人物' | '玩具' | '植物';
 }
 
 const list: Data[] = [
@@ -76,6 +76,31 @@ const list: Data[] = [
     name: '大黄蜂',
     image: 'assets/images/bumblebee.png',
     category: '玩具'
+  }, {
+    id: 'lotus',
+    name: '莲花',
+    image: 'assets/images/lotus.webp',
+    category: '植物'
+  }, {
+    id: 'sunflower',
+    name: '向日葵',
+    image: 'assets/images/sunflower.webp',
+    category: '植物'
+  }, {
+    id: 'apple',
+    name: '苹果',
+    image: 'assets/images/apple.webp',
+    category: '水果'
+  }, {
+    id: 'grape',
+    name: '葡萄',
+    image: 'assets/images/grape.webp',
+    category: '水果'
+  }, {
+    id: 'watermelon',
+    name: '西瓜',
+    image: 'assets/images/watermelon.webp',
+    category: '水果'
   }
 ];
 
